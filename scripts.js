dbdafab3df5eb49455de68efb5412eb87b544093
Jacky <jchoi53@mail.ccsf.edu>
2023-10-27 12:20:44 -0700

    finish js for rps

--- scripts.js
+++ scripts.js
@@ -1,14 +1,28 @@
 let playerSelection = prompt().toLowerCase();
 let computerSelection = getComputerChoice();
-
-console.log(playRound(playerSelection, computerSelection));
+alert("Computer chooses " + (computerSelection) +"." + " Player chooses " + playerSelection + ".");
+alert(playRound(playerSelection, computerSelection));
 
 function getComputerChoice() {
-    return "paper";
+    let randomNum = Math.floor(Math.random() * 3) + 1; //gives arandom number between 1, 2, or 3
+    if(randomNum == 3) {
+        return "rock";
+    }
+    else if(randomNum == 2) {
+        return "scissor";
+    }
+    else return "paper";
 }
 
 function playRound(playerSelection, computerSelection) {
-    if(playerSelection === "rock" && computerSelection == "paper") {
+    if((playerSelection === "rock" && computerSelection === "paper") || 
+    (playerSelection === "paper" && computerSelection === "scissor") || 
+    (playerSelection === "scissor" && computerSelection === "rock")) {
         return "Computer Wins";
+    }
+    else if((playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")) {
+        return "Player Wins";
     }
 }
